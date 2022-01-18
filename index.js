478b168122977e7adea484087b3da0fd14318f77
Guilherme Vasconcellos <43115561+GuilhermeVasconcellos@users.noreply.github.com>
2022-01-18 14:51:14 -0300

    Reativando banco de dados e adicionando URL do MongoDB

--- index.js
+++ index.js
@@ -3,19 +3,15 @@ const { MongoClient, ObjectId } = require("mongodb");
 
 const url = "mongodb://localhost:27017";
 // const url = "mongodb+srv://admin:hd2rV5duoPrrIi3t@cluster0.jup2c.mongodb.net/";
+const url = "mongodb+srv://admin:<cbc310864>@cluster0.7x2sd.mongodb.net";
 
 const dbName = "ocean_bancodados_18_01_2022";
 
 async function main() {
     // Conexão com o Banco de Dados
-/*
     const client = await MongoClient.connect(url);
-
     const db = client.db(dbName);
-
     const collection = db.collection("herois");
-*/
-    const collection = undefined;
     
     // Aplicação em Express
 
